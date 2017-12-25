70d823f4d534828de4a78f662cd365520ee7153d
shiyuan2he <shiyuan4work@sina.com>
2017-12-25 22:33:25 +0800

    提交忽略文件

--- crm-server/src/main/java/com/hsy/crm/server/CmsServerApplication.java
+++ crm-server/src/main/java/com/hsy/crm/server/CmsServerApplication.java
@@ -2,18 +2,21 @@ package com.hsy.crm.server;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @author heshiyuan
  * @description <p></p>
  * @path springboot.demo/com.hsy.springboot.demo.springboot.jdbc
- * @date ${data} ${time}
  * @github http://github.com/shiyuan2he
  * @email shiyuan4work@sina.com
  * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
  * @price ¥5    微信：hewei1109
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableHystrix
 public class CmsServerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(CmsServerApplication.class)
